6eba19eb3c9eafe3291de497f1ca842e084abed5
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-02-01 19:42:22 +0200

    Add files via upload

---
+++ Personal_Computer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Персональный компьютер
+    /// </summary>
+    internal class Personal_Computer
+    {
+        /// <summary>
+        /// название модели
+        /// </summary>
+        public string Model;
+        /// <summary>
+        /// тактовая частота процессора
+        /// </summary>
+        public double ProcessorFrequency;
+        /// <summary>
+        /// обьем оперативной памяти
+        /// </summary>
+        public int RAM;
+        /// <summary>
+        /// обьем жесткого диска
+        /// </summary>
+        public int HDD;
+        
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        public Personal_Computer() { }
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="model"></param>
+        public Personal_Computer(string model)
+        {
+            this.Model = model;
+        }
+        /// <summary>
+        /// Конструктор с 2 параметрами
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="ProcessorFrequency"></param>
+        public Personal_Computer(string model, double processor_frequency) : this(model)
+        {
+            this.ProcessorFrequency = processor_frequency;
+        }
+        /// <summary>
+        /// Конструктор с 3 параметрами
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="processor_frequency"></param>
+        /// <param name="ram"></param>
+        public Personal_Computer(string model, double processor_frequency, int ram) : this(model, processor_frequency)
+        {
+            RAM = ram;
+        }
+        /// <summary>
+        /// Конструктор с 4 параметрами
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="processor_frequency"></param>
+        /// <param name="ram"></param>
+        /// <param name="hdd"></param>
+        public Personal_Computer(string model, double processor_frequency, int ram, int hdd) : this(model, processor_frequency, ram)
+        {
+            HDD = hdd;
+        }
+
+        /// <summary>
+        /// Вывод информации
+        /// </summary>
+        public void Info()
+        {
+            Console.WriteLine($"Название модели: {Model}, тактовая частота  процессора (ГГц): {ProcessorFrequency}," +
+                $" обьем оперативной памяти(ГБ): {RAM}, обьем жесткого диска(ГБ): {HDD}");
+        }
+
+        /// <summary>
+        /// Вывод информации через метод ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Название модели: {Model}, тактовая частота  процессора (ГГц): {ProcessorFrequency}," +
+                $" обьем оперативной памяти(ГБ): {RAM}, обьем жесткого диска(ГБ): {HDD}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод через программу 
+            Personal_Computer pc1 = new Personal_Computer();
+            pc1.Info();
+            pc1.Model = "ASUS";
+            pc1.ProcessorFrequency = 6.5;
+            pc1.RAM = 32;
+            pc1.HDD = 1024;
+            
+            //Ввод через консоль
+            Console.WriteLine("Введите модель ПК:");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Введите тактовую частоту процессора (ГГц):");
+            double processorFrequency = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите объем оперативной памяти (ГБ):");
+            int ram = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите объем жесткого диска (ГБ):");
+            int hdd = int.Parse(Console.ReadLine());
+
+            Personal_Computer pc2 = new Personal_Computer(model, processorFrequency, ram, hdd);
+            Console.WriteLine($"{pc2.ToString()}");
+
+            Console.Read();
+        }
+    }
+}
